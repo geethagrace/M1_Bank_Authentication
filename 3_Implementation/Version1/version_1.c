497737e944c7a299c68ea50d468e292e7ed21660
geethagrace <101448322+geethagrace@users.noreply.github.com>
2022-03-27 21:28:04 +0530

    Update version_1.c

--- 3_Implementation/Version1/version_1.c
+++ 3_Implementation/Version1/version_1.c
@@ -2,116 +2,119 @@
 #include<string.h>
 int split(char *a)
 {
-char user[100],password[100],chkuser[3][100];
-char all_user[10][100];
-char da[100];
-int y,e=0,n=0,r,d=1,dr=0;
+	char user[100],password[100],chkuser[3][100];
+	char all_user[10][100];
+	char da[100];
+	int y,e=0,n=0,r,d=1,dr=0;
+    printf("\n welcome to login section....\n ");
+	printf("Enter the username :");
+	scanf("%s",user);
+	printf("Enter the password :");
+	scanf("%s",password);
+	for(y=0;a[y]!=NULL;y++)
+	{
+		if(a[y]=='\n')
+		{
+			da[n]=NULL;
+			strcpy(all_user[e++],da);
+			strcpy(da,"");
+			n=0;
+		}
+		else
+		{
+			da[n++]=a[y];
+		}
+	}
 
-printf("enter the username");
-scanf("%s",user);
-printf("enter the password");
-scanf("%s",password);
-for(y=0;a[y]!=NULL;y++)
-{
-if(a[y]=='\n')
-{
-da[n]=NULL;
-strcpy(all_user[e++],da);
-strcpy(da,"");
-n=0;
-}
-else
-{
-da[n++]=a[y];
-}
-}
+    for(y=0;y<e;y++)
+	{
+	dr=0,r=0;
+		for(n=0;all_user[y][n]!=NULL;n++)
+		{
+			if(all_user[y][n]==' ')
+			{
+			     chkuser[dr][r]=NULL;
+			     dr++;
+			     r=0;
+			}
+			else
+			{
+			 chkuser[dr][r]=all_user[y][n];
+			 r++;
+			}
+		}
 
+	if(strcmp(chkuser[0],user)==0 && strcmp(chkuser[1],password)==0)
+	{
+		printf("\n\n%s....Thank you for login",user);
+		d=0;
+		break;
+	}
 
-for(y=0;y<e;y++)
-{
-dr=0,r=0;
-for(n=0;all_user[y][n]!=NULL;n++)
-{
-if(all_user[y][n]==' ')
-{
-    chkuser[dr][r]=NULL;
-    dr++;
-    r=0;
-}
-else
-{
-chkuser[dr][r]=all_user[y][n];
-r++;
-}
-}
-
-if(strcmp(chkuser[0],user)==0 && strcmp(chkuser[1],password)==0)
-{
-printf("%s Thank you for login",user);
-d=0;
-break;
-}
-
-}
-return d;
+	}
+	return d;
 }
 void login()
 {
-FILE *file;
-char a;
-char z[100];
-int e=0;
-file=fopen("geetha.txt","r");
+	FILE *file;
+	char a;
+	char z[100];
+	int e=0;
+	file=fopen("Geetha.txt","r");
 
-do
-{
-a=fgetc(file);
-z[e]=a;
-e++;
-}while(a!=EOF);
-z[e]=NULL;
+    do
+	{
+		a=fgetc(file);
+		z[e]=a;
+		e++;
+	}while(a!=EOF);
+	z[e]=NULL;
        e=split(z);
        if(e==1)
        {
-printf("login failed");
+	        printf("\n\Illegal entry....Login Failed");
        }
 }
+
 void create()
 {
-FILE *file;
-char user[100],pass[100];
-file=fopen("hari.txt","a");
+	FILE *file;
+	char user[100],pass[100];
+	file=fopen("Geetha.txt","a");
 
-printf("enter the username");
-scanf("%s",user);
-printf("enter the password");
-scanf("%s",pass);
-strcat(user," ");
-strcat(user,pass);
-strcat(user," ");
-strcat(user,"500");
-strcat(user,"\n");
-printf("%s",user);
-fprintf(file,"%s",user);
+    printf("\n welcome to create an entry......\n");
+	printf("Enter the username :");
+	scanf("%s",user);
+	printf("Enter the password :");
+	scanf("%s",pass);
+	strcat(user," ");
+	strcat(user,pass);
+	strcat(user," ");
+	strcat(user,"500");
+	strcat(user,"\n");
+	printf("%s",user);
+	printf("\n ....entry created....\n");
+	fprintf(file,"%s",user);
 
-fclose(file);
+    fclose(file);
 }
 int main()
 {
-   int y;
-   printf("1.Create Account 2.Login account");
-   scanf("%d",&y);
-   if(y==1)
-  {
-     create();
-  }
-  else if(y==2)
- {
-    login();
- }
- else
- {
-    printf("wrong entry");
-  }
+	int y;
+	printf("AUTHENTICATION SYSTEM\n");
+	printf("1.Create Account\n2.Login account\n");
+	printf("Enter your choice : ");
+	scanf("%d",&y);
+	switch(y)
+	{
+	    case 1:
+		   create();
+		   break;
+        case 2:
+            login();
+            break;
+        default:
+            printf("\n wrong entry.......");
+	}
 }
 
