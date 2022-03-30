5597a9816907b5778da0750ecca336e6116e979f
geethagrace <101448322+geethagrace@users.noreply.github.com>
2022-03-30 23:43:34 +0530

    Create version1.h

---
+++ 3_Implementation/inc/version1.h
@@ -0,0 +1,30 @@
+/**
+ * @file  version1.h
+ * @author Geetha
+ * @version 0.1
+ * @date 2022-03-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#ifndef __VERSION1_H__
+#define __VERSION1_H__
+
+
+/**
+ * Include files
+ */ 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * Function declarations
+ */
+
+void create();
+void login();
+
+#endif /** __VERSION1_H__ */
