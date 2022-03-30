60643f54170759d4d5d4202df6ac7a050f3916ba
geethagrace <101448322+geethagrace@users.noreply.github.com>
2022-03-30 23:30:23 +0530

    Create project_config.c

---
+++ 3_Implementation/src/project_config.c
@@ -0,0 +1,46 @@
+#include<project_config.h>
+
+void login()
+{
+	FILE *file;
+	char a;
+	char z[100];
+	int e=0;
+	file=fopen("geetha.txt","r");
+
+
+	do
+	{
+		a=fgetc(file);
+		z[e]=a;
+		e++;
+	}while(a!=EOF);
+	z[e]=NULL;
+       e=split(z);
+       if(e==1)
+       {
+	       printf("\n ...login failed");
+       }
+}
+
+void create()
+{
+	FILE *file;
+	char user[100],pass[100];
+	file=fopen("geetha.txt","a");
+
+	printf("Enter the username : ");
+	scanf("%s",user);
+	printf("Enter the password : ");
+	scanf("%s",pass);
+	strcat(user," ");
+	strcat(user,pass);
+	strcat(user," ");
+	strcat(user,"500");
+	strcat(user,"\n");
+	printf("%s",user);
+	fprintf(file,"%s",user);
+
+
+	fclose(file);
+}
