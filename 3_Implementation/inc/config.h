107acbc93157b451bd3c09b342bee34b8b61a7cf
geethagrace <101448322+geethagrace@users.noreply.github.com>
2022-03-30 23:38:43 +0530

    Create config.h

---
+++ 3_Implementation/inc/config.h
@@ -0,0 +1,30 @@
+/**
+ * @file project_config.h
+ * @author Geetha
+ * @version 0.1
+ * @date 2022-03-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+
+
+/**
+ * Include files
+ */ 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * Function declarations
+ */
+void create();
+void login();
+
+
+#endif /* __PROJECT_CONFIG_H__ */
